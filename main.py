6222031d53d9ae64925151c551ec433d072785e4
GGsmou <svyatoslav.golovin.2005@gmail.com>
2022-10-28 15:16:57 +0300

    z

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import threading
 import datetime
 from xmldiff import main as xmldiffM
 
+comand = 0
+
 def load_inf():
     tree = ET.parse('data.xml')
     return tree, tree.getroot()
@@ -11,6 +13,25 @@ def save_inf(root):
     with open('data.xml.temp', 'wb') as f:
         b_tree = ET.tostring(root, encoding='UTF-8', xml_declaration=True)
         f.write(b_tree)
+
+def input_proces():
+    global comand
+    comand_inp = input('>    ')
+
+    try:
+        comand_inp = int(comand_inp)
+
+        if comand_inp == comand:
+            return 'same'
+        elif comand_inp in [0, 1, 2]:
+            print('Не коректне значення')
+            return 'error'
+        else:
+            comand = comand_inp
+            return 'ok'
+    except NameError:
+        print('Не коректне значення')
+        return 'error'
 
 def convert_month(num):
     month_dic = {'01':'січня',
@@ -50,30 +71,33 @@ def main():
             threading.Timer(5.0, chage_checker).start()
         else:
             main()
+    if comand == 0:
+        for i in range(len(root)):
+            datetime_str = root[i].attrib['value']
+            print(f'{datetime_str[-2:]} {convert_month(datetime_str[5:7])} ({get_day(datetime_str)})')
 
-    for i in range(len(root)):
-        datetime_str = root[i].attrib['value']
-        print(f'{datetime_str[-2:]} {convert_month(datetime_str[5:7])} ({get_day(datetime_str)})')
+            for c in range(len(root[i][0])):
+                film_name = root[i][0][c].attrib["name"]
+                tab_count = 20 - len(film_name)
+                tabs = ' ' * tab_count
+                time_str = ''
 
-        for c in range(len(root[i][0])):
-            film_name = root[i][0][c].attrib["name"]
-            tab_count = 20 - len(film_name)
-            tabs = ' ' * tab_count
-            time_str = ''
+                for b in range(len(root[i][0][c][0])):
+                    if root[i][0][c][0][b].attrib["reserv"] == 'F':
+                        div_symb = ' | '
+                        if time_str == '': div_symb = ''
+                        time_str += f'{div_symb}{root[i][0][c][0][b].attrib["time"]}'
+                if time_str == '': time_str = 'Всі білети заброньовано'
 
-            for b in range(len(root[i][0][c][0])):
-                div_symb = ''
-                if b != 0: div_symb = ' | '
-                time_str += f'{div_symb}{root[i][0][c][0][b].attrib["time"]}'
+                print(f'    {film_name}{tabs}{time_str}')
+            print('')
+        print('Команди:')
+        print('1. Забронювати сеанс')
+        print('2. Список заброньованих сеансів')
 
-            print(f'    {film_name}{tabs}{time_str}')
-        print('')
-    print('Команди:')
-    print('1. Забронювати сеанс')
-    print('2. Список заброньованих сеансів')
+        threading.Thread(target=input_proces).start()
 
     chage_checker()
 
 if __name__ == '__main__':
-    comand = 0
     main()
